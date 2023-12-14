684b2f63cee9da4ead604d6551f54cbda15c09df
Sidaredd <120279550+Sidaredd@users.noreply.github.com>
2023-12-14 09:39:20 +0300

    Experimental Change

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -46,6 +46,68 @@ function Main() {
   };
   return (
     <>
+  <style>
+      
+    .card {
+      width: 100%;
+      max-width: 400px;
+      padding: 20px;
+      border-radius: 10px;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+      background-color: #fff;
+    }
+
+    h1 {
+      text-align: center;
+      color: #007bff;
+    }
+
+    form {
+      margin-top: 20px;
+    }
+
+    label {
+      display: block;
+      margin-bottom: 8px;
+      color: #333;
+    }
+
+    input {
+      width: 100%;
+      padding: 10px;
+      margin-bottom: 15px;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      box-sizing: border-box;
+    }
+
+    button {
+      width: 100%;
+      padding: 10px;
+      background-color: #007bff;
+      color: #fff;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+
+    button:hover {
+      background-color: #0056b3;
+    }
+    select,
+    input {
+      width: 100%;
+      padding: 10px;
+      margin-bottom: 15px;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      box-sizing: border-box;
+    }
+
+    select {
+      background-color: #f8f9fa;
+    }
+  </style>
       <div>
         <h1>Hoşgeldin {user.name}</h1>
       </div>
@@ -74,13 +136,17 @@ function Main() {
             Gerekli Belge:
             <input type="file" name="file" onChange={handleFileChange} />
           </label>
+           <label for="kategori">Kategori:</label>
+              <select id="kategori" name="kategori">
+              <option value="1">Seçenek 1</option>
+              <option value="2">Seçenek 2</option>
+              <option value="3">Seçenek 3</option>
+              <option value="4">Seçenek 4</option>
+              </select>
           <br />
 
           <button type="submit">Yükle</button>
         </form>
-      </div>
-      <div>
-        <ActivityTable></ActivityTable>
       </div>
     </>
   );
