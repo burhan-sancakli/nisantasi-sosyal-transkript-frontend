254a77e255bba75afd401c55300393f6e73f03b3
Sidaredd <120279550+Sidaredd@users.noreply.github.com>
2023-12-14 08:59:06 +0300

    Update page.tsx

--- src/app/login/staff/page.tsx
+++ src/app/login/staff/page.tsx
@@ -29,19 +29,10 @@ function Staff() {
       </div>
 
       <div className="row mt-3 justify-content-between">
-        <div className="col-12 col-md-6 col-lg-auto d-flex align-items-center">
-          <a
-            href="#"
-            id="sifreGonder"
-            className="btn btn-sm fw-semibold w-100 w-md-auto"
-          >
-            Şifremi Unuttum
-          </a>
-        </div>
         <div className="col-12 col-md-6 col-lg-auto">
           <button
             type="submit"
-            className="btn btn-primary-2 fw-semibold w-100 w-md-auto"
+            className="btn btn-primary-2 fw-semibold w-100 w-md-auto ml-auto"
           >
             <span style={{ viewTransitionName: "login-button" }}>
               GİRİŞ YAP
